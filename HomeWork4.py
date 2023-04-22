eaf546a983164e58bfbb4a26a15adeeac2153225
Valensia1226 <valensia16@yandex.ru>
2023-04-22 16:11:13 +0300

    homework4 is done

--- HomeWork4.py
+++ HomeWork4.py
@@ -1,33 +1,58 @@
+import math
+
+def PrimeDividers(N):
+    dividerList = list()
+    flag = True
+    if N == 1: return 1
+    for i in range(2, N//2):
+        if N % i == 0:
+            flag = True
+            for j in range(2, i):
+                if i % j == 0: 
+                    flag = False
+                    break
+            if flag == True: dividerList.append(i)
+    return dividerList
+                
 def Task1(N):
-    i = 2
-    count = 0
-    while N > 1:
-        if N % 2 == 0: 
-            count += 1
-            N = N // 2
-            print(2, end=' ')
-            continue
-        if N % 3 == 0:
-            count += 1
-            N = N // 3
-            print(3, end=' ')
-            continue
-        if N % 5 == 0:
-            count += 1
-            N = N // 5
-            print(5, end=' ')
-            continue
-        if N % 7 == 0:
-            count += 1
-            N = N // 7
-            print(7, end=' ')
-            continue
-        if N % 5 == 0:
-            count += 1
-            N = N // 5
-            print(5, end=' ')
-            continue
+    primeDivider = PrimeDividers(N)
+    primeFactors = list()
+    primeFactors.append(primeDivider[0])
+    N = N // primeDivider[0]
+    count = 1
+    for i in range(len(primeDivider)):
+        for j in range(N//2):
+            if N % primeDivider[i] == 0: 
+                N = N // primeDivider[i]
+                primeFactors.append(primeDivider[i])
+                count = count + 1
+                continue
+            else: break
+    print(primeFactors)
     print()
     print(f'количество = {count}')
 
-Task1(60)
+def Task2():
+    list1 = ['Сливочное', 'Бурёнка', 'Вафелька', 'Сладкоежка']
+    list2 = ['Сливочное', 'Вафелька', 'Сладкоежка']
+    length1 = len(list1)
+    length2 = len(list2)
+    if length1 == length2: print('Все на месте') #в предположении, что бренды на складе и в ассортименте не могут быть совсем разными
+    for i in range(length1):
+        flag = False
+        for j in range(length2):
+            if list1[i] == list2[j]: 
+                flag = True
+                break
+        if flag == False: print(list1[i])
+
+def Task3(number):
+    accuracy = str(number).split('.')
+    print(accuracy)
+    length = len(accuracy[1])
+    pi = math.pi
+    rez = str(pi)
+    print(number)
+    print(rez[0:length + 2])
+
+Task3(0.1)
