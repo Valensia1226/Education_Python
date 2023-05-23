c6b1248d03461be5c7c8d8225cf9a84f50a246b8
Valensia1226 <valensia16@yandex.ru>
2023-05-23 19:43:08 +0300

    fix hw7

--- HomeWork7.py
+++ HomeWork7.py
@@ -30,25 +30,3 @@ def Task2():
 
     message()
     sum(3, 5)
-
-
-import telebot
-import requests
-import time
-
-bot = telebot.TeleBot("5724189873:AAHs8nNm8yqrRZcY1eL_X5tHwwScje3GHVA") 
-
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
-
-@bot.message_handler(content_types=['text'])
-def greetings(message):
-	text = message.text
-	if text == 'котик':
-		req = requests.get(f'https://cataas.com/cat?{time.time()}')
-		bot.send_photo(message.from_user.id, req.content)
-    elif 'привет' in text:
-        bot.reply_to(message, f'Привет, {message.from_user.first_name}')
-
-bot.polling()
